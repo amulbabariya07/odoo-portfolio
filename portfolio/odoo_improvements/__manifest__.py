ccdcf0ed75ffe08dfdf507858a5109228f09f5bb
Amul Babariya <amulbabariya07@Amuls-mac.local>
2026-07-11 13:52:03 +0530

    [IMP] Improvement module add

---
+++ portfolio/odoo_improvements/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Improvements',
+    'version': '19.0.1.0.0',
+    'summary': 'General Odoo Improvements',
+    'description': """
+        A module for general Odoo improvements and customizations.
+    """,
+    'author': 'Amul Babariya',
+    'depends': ['base'],
+    'data': [],
+    'assets': {},
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
